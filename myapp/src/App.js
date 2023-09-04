6a6d82cb1c3b20c2dbf09d1c13397f0473ee2ccd
juan <crumbled23@gmail.com>
2023-09-04 22:57:58 +0900

    todo list

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,21 +1,30 @@
-import { useState, useEffect } from "react";
-
-function Hello() {
-  useEffect(()=> {
-   console.log("hi :)") 
-   return () => console.log("bye :(") //cleanup function : component가 destroy될때 실행
-  }, []);
-  return <h1>Hello</h1>
-}
+import { useState } from "react";
 
 
 function App() {
-  const [showing, setShowing] =useState(false);
-  const onClick = () => setShowing((prev) => !prev);
+  const [toDo, setToDo] =useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if(toDo === "") {
+      return;
+    }
+    setToDos((prev) => [toDo, ...prev]);
+    setToDo("");
+  }
+  console.log(toDos);
   return (
     <div className="App">
-      {showing ? <Hello/> : null}
-      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+      <h1>My To Dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input value={toDo} onChange={onChange} type="text" placeholder="Write your to do..."></input>
+        <button>Add To Do</button>
+      </form>
+      <hr />
+      <ul>
+        {toDos.map((item, index)=> <li key={index}>{item.toUpperCase()}</li>)}
+      </ul>
     </div>
   );
 }
